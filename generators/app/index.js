a9d7e81729cbd109a2b4a1004a472b0527d7c476
Jonas Friedmann <j@frd.mn>
2016-01-16 19:25:43 +0100

    Disable require for yosay

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,8 +1,9 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
 var mkdirp = require('mkdirp');
+
+// var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
   // prompting: function () {
