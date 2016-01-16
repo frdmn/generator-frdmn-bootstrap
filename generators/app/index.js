bec6ccda5cbbb645ecbcf2a7912e1c1ee5344af3
Jonas Friedmann <j@frd.mn>
2016-01-16 18:50:56 +0100

    Disable prompting section

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,28 +4,50 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+  // prompting: function () {
+  //   var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the perfect ' + chalk.red('generator-frdmn-bootstrap') + ' generator!'
-    ));
+  //   // Have Yeoman greet the user.
+  //   this.log(yosay(
+  //     'Welcome to the perfect ' + chalk.red('generator-frdmn-bootstrap') + ' generator!'
+  //   ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+  //   var prompts = [{
+  //     type: 'confirm',
+  //     name: 'someOption',
+  //     message: 'Would you like to enable this option?',
+  //     default: true
+  //   }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+  //   this.prompt(prompts, function (props) {
+  //     this.props = props;
+  //     // To access props later use this.props.someOption;
 
-      done();
-    }.bind(this));
-  },
+  //     done();
+  //   }.bind(this));
+  // },
+
+  writing: {
+    git: function () {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+    },
+
+    gulpfile: function () {
+      this.fs.copyTpl(
+        this.templatePath('gulpfile.js'),
+        this.destinationPath('gulpfile.js')
+      );
+    },
+
+    packageJSON: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+    },
 
   writing: function () {
     this.fs.copy(
